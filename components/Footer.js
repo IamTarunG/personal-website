5a714bb1dc30c492fd73626d30d5fe76eb47bf2b
IamTarunG <tarungudipalli5@gmail.com>
2022-10-09 12:41:18 +0530

    Added LinkedIn

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 
             <hr className="w-full border-1 border-gray-800 dark:border-gray-800 mb-8" />
             <NowPlaying />
-            <div className="py-5 w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3 ">
+            <main className="py-5 w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3 ">
                 <div className="flex flex-col space-y-4">
                     <Link href="/">
                         <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
@@ -31,27 +31,32 @@ function Footer() {
                 <div className="flex flex-col space-y-4">
 
                     <Link href="https://github.com/IamTarunG">
-                        <a className="text-gray-500 hover:text-gray-600 transition">
+                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
                             GitHub
                         </a>
                     </Link>
                     <Link href="https://unsplash.com/@tarun_gudipalli">
-                        <a className="text-gray-500 hover:text-gray-600 transition">
+                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
                             Unsplash
                         </a>
                     </Link>
                     <Link href="https://twitter.com/_tarungudipalli">
-                        <a className="text-gray-500 hover:text-gray-600 transition">
+                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
                             Twitter
                         </a>
                     </Link>
                     <Link href="https://www.instagram.com/tarun_gudipalli/">
-                        <a className="text-gray-500 hover:text-gray-600 transition">
+                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
                             Instagram
                         </a>
                     </Link>
+                    <Link href="https://www.linkedin.com/in/tarun-gudipalli-95130a120/">
+                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
+                            LinkedIn
+                        </a>
+                    </Link>
                 </div>
-            </div>
+            </main>
         </footer>
     )
 }
