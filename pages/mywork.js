fe0b4833e58e92adf53e45bf5474f626fa995122
IamTarunG <tarungudipalli5@gmail.com>
2022-11-05 14:11:54 +0530

    removed project

--- pages/mywork.js
+++ pages/mywork.js
@@ -13,7 +13,6 @@ function MyWork() {
             </section>
 
             <section className='grid gap-4 grid-cols-1 sm:grid-cols-2 pb-10 w-full px-20'>
-                <ProjectCard title={"E-Authentication System"} description="The E-Authentication System provides security to both the user and resources using QR code and OTP" link={"https://eauth-lemon.vercel.app/"} />
                 <ProjectCard title={"Project Management"} description="The Project Management application provides ease of use to the clients to manage the projects. the client can add, edit and delete the project.the project is built using ReactJS, Express, Mongo DB, NodeJS And GraphQL." link={"https://projectmgnt.vercel.app/"} />
                 <ProjectCard title={"Travel Experience"} description="The Travel Experience application provides users to upload their travel experiences and one can edit and delete their experiences.this application is built using reactjs, react-redux, nodejs, express and mongo db and for styling it uses tailwind css." link={"https://travel-experience.vercel.app/"} />
                 <ProjectCard title={"TextUtils"} description="The Textutils application provides the utilities to convert the text into uppercase, lowercase. one can also change the theme from light to dark and vice-versa. this application is made using reactjs" link={"https://utilitiestext.netlify.app/"} />
