bd3a5ed5687b16018a6a8e1706fb111ac8ed9ded
Tarun <74412769+IamTarunG@users.noreply.github.com>
2022-12-05 17:07:18 +0530

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div>
       <main className="h-screen flex justify-center items-center">
-        <HeadContent title={"Tarun Gudipalli - Web Developer and Blender Enthusiast"} description="Web Developer and Blender Enthusiast" />
+        <HeadContent title={"Tarun Gudipalli - Web Developer"} description="I'm a Web developer with a passion. In my free time, I enjoy building custom templates for Notion to help others streamline their workflow" />
         <section className="flex justify-evenly items-center flex-col my-20 h-64">
           <Image
             alt="Tarun Gudipalli"
