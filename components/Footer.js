48b5550749cda2a30ea276a0917479056b35bc68
Tarun <74412769+IamTarunG@users.noreply.github.com>
2022-12-09 15:15:01 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -45,11 +45,7 @@ function Footer() {
                             Unsplash
                         </a>
                     </Link>
-                    <Link href="https://medium.com/@tarungudipalli">
-                        <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
-                            Medium
-                        </a>
-                    </Link>
+                  
                     <Link href="https://twitter.com/_tarungudipalli">
                         <a className="text-gray-500 hover:text-gray-600 transition" target={"_blank"}>
                             Twitter
